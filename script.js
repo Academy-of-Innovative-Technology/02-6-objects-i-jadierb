964fd566b45c444aff066f46e8698db9cac6935b
jadier <144461536+jadierb@users.noreply.github.com>
2026-01-15 22:36:04 +0000

    created my own object

--- script.js
+++ script.js
@@ -21,8 +21,28 @@ var elonMusk = {
 	 "Queens College",
 	 "Stanford University",
 	 "Wharton School of the University of Pennslvaina"
-	]
+	],
 	img:
-	"https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/220px-Elon_Musk_Royal_Society_%28crop2%29.jpg"
+	"https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/220px-Elon_Musk_Royal_Society_%28crop2%29.jpg",
 	wiki: "https://en.wikipedia.org/wiki/Elon_Musk"
 };
+
+var MichealJordan = {
+	firstName: "micheal",
+	middleName: "jeffrey",
+	lastName: "jordan",
+	age: 63,
+	education: [
+		"Emsley A. Laney High School",
+		"University of North Carolina at Chapel Hill"
+
+	],
+	occupaction: "basketball player",
+	awards: ["6x NBA champion", "5x NBA most valuable player", "14x NBA All-star"],
+	hometown: "Brooklyn, New York City",
+	img:
+	"https://people.com/thmb/fBzOWOOX7IoYdaQePshE8109tlg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/Michael-Jordan-attends-the-13th-annual-Michael-Jordan-Celebrity-Invitational-gala-051225-d72a742c85c34985b8e731d39ebe5717.jpg",
+	wiki: "https://en.wikipedia.org/wiki/micheal_jordan"
+};
+
+
