75b3d10bbbaaee8e739e6380f1bd21b76b9f0a19
jadier <144461536+jadierb@users.noreply.github.com>
2026-01-15 15:13:42 +0000

    displayed the full name and the education, created the wikipedia link

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 var theranos = {
-	first: "Elizabeth",
-	last: "Holmes",
-	year: 1984,
-	college: [
+
+	firstname: "Elizabeth Holmes",
+	lastname: "Holmes",
+	age: 40,
+
+
+education: [
 		"St. John University", "Stanford University"
 	],
 	img: "https://startupi.com.br/wp-content/uploads/2023/08/20160711elizabeth-holmes.jpg",
     wiki: "https://en.wikipedia.org/wiki/Elizabeth_Holmes"
 };
+
+var elonMusk = {
+	firstName: "Elon",
+	middleName: "Reeve",
+	
+}
